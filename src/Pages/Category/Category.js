681797b56b5f0f2a1537e5e51ef0d7996fbb9e59
laboneeaktar <labonipub@gmail.com>
2022-10-20 11:38:41 +0600

    news details add

--- src/Pages/Category/Category.js
+++ src/Pages/Category/Category.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
+import NewsSummaryCart from "../Shared/NewsSummaryCart/NewsSummaryCart";
 
 const Category = () => {
   const categoryNews = useLoaderData();
@@ -7,6 +8,9 @@ const Category = () => {
   return (
     <div>
       <h4>This category has news: {categoryNews.length}</h4>
+      {categoryNews.map((news) => (
+        <NewsSummaryCart key={news._id} news={news}></NewsSummaryCart>
+      ))}
     </div>
   );
 };
--- src/Pages/News/News.js
+++ src/Pages/News/News.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { Link, useLoaderData } from "react-router-dom";
 
 const News = () => {
+  const news = useLoaderData();
+  const { category_id, title, details, image_url } = news;
   return (
     <div>
-      <h3>this is news</h3>
+      <Card>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{details}</Card.Text>
+          <Link to={`/category/${category_id}`}>
+            <Button variant="success">Go News Category</Button>
+          </Link>
+        </Card.Body>
+      </Card>
     </div>
   );
 };
